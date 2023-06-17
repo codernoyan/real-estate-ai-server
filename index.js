b0ecfd6b490decb536d5a31276d651ab1a61fb0a
codernoyan <coder.noyan@gmail.com>
2023-06-17 18:29:52 +0600

    added .evn file and morgan for route check

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const lol = require('./routes/lol');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use('/', lol);
+app.use(morgan('dev'));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
