1c322fdcff60254ea845664dbaed6115f98d7544
codernoyan <coder.noyan@gmail.com>
2023-06-17 19:06:02 +0600

    added mongodb configaration

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const morgan = require('morgan');
 require('dotenv').config();
+const morgan = require('morgan');
+const { dbConnect } = require('./mongodb/mongodb.config');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -10,8 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// run mongodb
+dbConnect();
+
+// default get route
 app.get('/', (req, res) => {
   res.send('Real Estate server is running');
-})
+});
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
