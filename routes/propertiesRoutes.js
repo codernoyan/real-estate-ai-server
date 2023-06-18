a1ca91786bcbaed3e11988775187719fb0d0a934
codernoyan <coder.noyan@gmail.com>
2023-06-19 01:41:35 +0600

    added get a single property route

--- routes/propertiesRoutes.js
+++ routes/propertiesRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { propertiesCollection } = require('../collections/collections');
+const { ObjectId } = require('mongodb');
 const router = express.Router();
 
+// get all properties
 router.get('/getProperties', async (req, res) => {
   try {
     const query = {};
@@ -20,4 +22,40 @@ router.get('/getProperties', async (req, res) => {
   }
 });
 
+// post a property
+router.post('/addProperty', async (req, res) => {
+  try {
+    const propertyData = req.body;
+    const result = await propertiesCollection.insertOne(propertyData);
+    // send response
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      error: err.message,
+    });
+  }
+});
+
+// get a property
+router.get('/getProperty/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await propertiesCollection.findOne(query);
+    // send response
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      error: err.message,
+    })
+  }
+})
 module.exports = router;
