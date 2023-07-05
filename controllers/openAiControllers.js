5e38969fc9a1bd6c8f5336ea30c3b354e01c9651
codernoyan <coder.noyan@gmail.com>
2023-07-06 00:58:23 +0600

    max token increased of text model

--- controllers/openAiControllers.js
+++ controllers/openAiControllers.js
@@ -37,7 +37,7 @@ const generateText = async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
-      max_tokens: 256,
+      max_tokens: 1000,
       temperature: 0,
     });
     const createdText = response.data.choices[0].text;
